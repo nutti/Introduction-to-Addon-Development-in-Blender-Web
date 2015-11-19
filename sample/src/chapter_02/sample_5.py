f7900fe02a480d484a4c18cbee2ab89d184ffad4
Nutti <nutti.metro@gmail.com>
2015-11-19 21:38:35 +0900

    WIP: ch.2 sect.5

---
+++ sample/src/chapter_02/sample_5.py
@@ -0,0 +1,150 @@
+```py:sample_5.py
+import bpy
+from bpy.props import FloatVectorProperty, EnumProperty
+from mathutils import Vector
+
+bl_info = {
+    "name": "サンプル5: オブジェクトを複製するアドオン",
+    "author": "Nutti",
+    "version": (1, 0),
+    "blender": (2, 75, 0),
+    "location": "Object > サンプル5: オブジェクトを複製するアドオン",
+    "description": "選択したオブジェクトを複製するアドオン",
+    "warning": "",
+    "support": "TESTING",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Object"
+}
+
+
+# EnumPropertyで表示したい項目リストを作成する関数
+def location_list_fn(scene, context):
+    items = [
+        ('3D_CURSOR', "3Dカーソル", "3Dカーソル上に配置します"),
+        ('ORIGIN', "原点", "原点に配置します")]
+    items.extend([('OBJ_' + o.name, o.name, "オブジェクトに配置します") for o in bpy.data.objects])
+
+    return items
+
+# 選択したオブジェクトを複製するアドオン
+class ReplicateObject(bpy.types.Operator):
+
+    bl_idname = "object.replicate_object"
+    bl_label = "オブジェクトの複製"
+    bl_description = "オブジェクトを複製します"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    location = EnumProperty(
+        name = "配置位置",
+        description = "複製したオブジェクトの配置位置",
+        items = location_list_fn
+    )
+
+    scale = FloatVectorProperty(
+        name = "拡大率",
+        description = "複製したオブジェクトの拡大率を設定します",
+        default = (1.0, 1.0, 1.0),
+        subtype = 'XYZ',
+        unit = 'LENGTH'
+    )
+
+    rotation = FloatVectorProperty(
+        name = "回転角度",
+        description = "複製したオブジェクトの回転角度を設定します",
+        default = (0.0, 0.0, 0.0),
+        subtype = 'AXISANGLE',
+        unit = 'ROTATION'
+    )
+
+    offset = FloatVectorProperty(
+        name = "オフセット",
+        description = "複製したオブジェクトの配置位置からのオフセットを設定します",
+        default = (0.0, 0.0, 0.0),
+        subtype = 'TRANSLATION',
+        unit = 'LENGTH'
+    )
+
+    src_obj_name = bpy.props.StringProperty()
+
+    def execute(self, context):
+        # bpy.ops.object.duplicate()実行後に複製オブジェクトが選択されるため、選択中のオブジェクトを保存
+        # context.active_object.name：選択中のオブジェクトの名前
+        for o in context.scene.objects:
+            if self.src_obj_name == o.name:
+                context.scene.objects.active = o
+                o.select = True
+                break
+            else:
+                o.select = False
+        # bpy.ops.object.duplicate()：オブジェクトの複製
+        bpy.ops.object.duplicate()
+        active_obj = context.active_object
+
+        # 複製したオブジェクトを配置位置に移動
+        # context.active_object.location：選択中のオブジェクトの位置
+        if self.location == '3D_CURSOR':
+            # context.scene.cursor_location：3Dカーソルの位置
+            # Shallow copyを避けるため、copy()によるDeep copyを実行
+            active_obj.location = context.scene.cursor_location.copy()
+        elif self.location == 'ORIGIN':
+            active_obj.location = Vector((0.0, 0.0, 0.0))
+        elif self.location[0:4] == 'OBJ_':
+            # bpy.data.objects：配置されているオブジェクトのリスト
+            active_obj.location = bpy.data.objects[self.location[4:]].location.copy()
+
+        # 複製したオブジェクトの拡大率を設定
+        # context.active_object.scale：選択中のオブジェクトの拡大率
+        active_obj.scale.x = active_obj.scale.x * self.scale[0]
+        active_obj.scale.y = active_obj.scale.y * self.scale[1]
+        active_obj.scale.z = active_obj.scale.z * self.scale[2]
+
+        # 複製したオブジェクトの回転角度を設定
+        # context.active_object.rotation_euler：選択中のオブジェクトの回転角度（ラジアン）
+        active_obj.rotation_euler.x = active_obj.rotation_euler.x + self.rotation[0]
+        active_obj.rotation_euler.y = active_obj.rotation_euler.y + self.rotation[1]
+        active_obj.rotation_euler.z = active_obj.rotation_euler.z + self.rotation[2]
+
+        # 複製したオブジェクトの最終位置を設定
+        active_obj.location = active_obj.location + Vector(self.offset)
+
+        self.report({'INFO'}, "サンプル 4: 「%s」を複製しました。" % self.src_obj_name)
+        print("サンプル 4: オペレーション「%s」が実行されました。" % self.bl_idname)
+
+        return {'FINISHED'}
+
+
+# メインメニュー
+class ReplicateObjectMenu(bpy.types.Menu):
+    bl_idname = "uv.replicate_object_menu"
+    bl_label = "オブジェクトの複製"
+    bl_description = "オブジェクトを複製します"
+
+    def draw(self, context):
+        layout = self.layout
+        # サブメニューの登録＋出力文字列の登録
+        for o in bpy.data.objects:
+            layout.operator(ReplicateObject.bl_idname, text=o.name).src_obj_name = o.name
+
+
+def menu_fn(self, context):
+    self.layout.separator()
+    self.layout.menu(ReplicateObjectMenu.bl_idname)
+
+
+def register():
+    bpy.utils.register_module(__name__)
+    bpy.types.VIEW3D_MT_object.append(menu_fn)
+    print("サンプル 5: アドオン「サンプル5」が有効化されました。")
+
+
+def unregister():
+    bpy.types.VIEW3D_MT_object.remove(menu_fn)
+    bpy.utils.unregister_module(__name__)
+    print("サンプル 5: アドオン「サンプル 5」が無効化されました。")
+
+
+if __name__ == "__main__":
+    register()
+
+```
