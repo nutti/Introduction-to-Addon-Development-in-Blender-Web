a7d5babe69555daff2fb983554c322e108305304
Nutti <nutti.metro@gmail.com>
2016-11-28 23:26:56 +0900

    Fix: sample 3-3

--- sample_raw/src/chapter_03/sample_3-3.py
+++ sample_raw/src/chapter_03/sample_3-3.py
@@ -1,6 +1,8 @@
 import bpy
 from bpy.props import BoolProperty, PointerProperty, IntProperty, EnumProperty
 import datetime
+import math
+
 
 bl_info = {
     "name": "サンプル3-3: オブジェクトモードとエディットモードでの作業時間を計測する",
@@ -17,24 +19,13 @@ bl_info = {
 }
 
 
-working_hour = {}
-
-def update_db():
-    # 全メッシュ型オブジェクトの取得
-    obj_list = [obj.name for obj in bpy.data.objects if obj.type == 'MESH']
-    # データベースアップデート
-    for o in obj_list:
-        if not o in working_hour.keys():
-            working_hour[o] = {}
-            working_hour[o]['OBJECT'] = 0
-            working_hour[o]['EDIT'] = 0
-
 # プロパティ
 class CWH_Properties(bpy.types.PropertyGroup):
     is_calc_mode = BoolProperty(
         name="作業時間計測中",
         description="作業時間計測中か？",
         default=False)
+    working_hour_db = {}    # 作業時間を保存するためのデータベース
 
 
 # 作業時間計測時の処理
@@ -43,42 +34,73 @@ class CalculateWorkingHours(bpy.types.Operator):
     bl_label = "作業時間計測"
     bl_description = "作業時間を計測します"
 
-    timer = None
+    timer = None    # タイマのハンドラ
+
 
     def __init__(self):
-        self.prev_time = 0.0
-        self.prev_obj = None
-        self.prev_mode = None
+        self.prev_time = 0.0        # __calc_delta()メソッドを呼び出した時の時間
+        self.prev_obj = None        # __calc_delta()メソッドを呼び出した時に選択していたオブジェクト
+        self.prev_mode = None   # __calc_delta()メソッドを呼び出した時のモード
+
 
     @staticmethod
     def handle_add(self, context):
         if CalculateWorkingHours.timer is None:
+            # タイマを追加
             CalculateWorkingHours.timer = context.window_manager.event_timer_add(
                 0.10, context.window)
+            # モーダルモードへの移行
             context.window_manager.modal_handler_add(self)
+
 
     @staticmethod
     def handle_remove(self, context):
         if CalculateWorkingHours.timer is not None:
+            # タイマを削除
             context.window_manager.event_timer_remove(CalculateWorkingHours.timer)
             CalculateWorkingHours.timer = None
 
+
+    # 前回の呼び出しからの時間差分を計算
     def __calc_delta(self, obj):
+        # 現在時刻を取得
         cur_time = datetime.datetime.now()
 
+        # オブジェクトやモードが異なっていた場合は無効とし、時間差分を0とする
         if (self.prev_obj != obj) or (self.prev_mode != obj.mode):
             delta = 0.0
         else:
             delta = (cur_time - self.prev_time).total_seconds()
 
+        # 情報をアップデート
         self.prev_time = cur_time
         self.prev_obj = obj
         self.prev_mode = obj.mode
 
         return delta
 
+
+    # データベースを更新
+    def __update_db(self, context):
+        props = context.scene.cwh_props
+
+        # 全メッシュ型オブジェクトの取得
+        obj_list = [obj.name for obj in bpy.data.objects if obj.type == 'MESH']
+        # データベースに存在しないオブジェクトをデータベースに追加
+        for o in obj_list:
+            if not o in props.working_hour_db.keys():
+                props.working_hour_db[o] = {}
+                props.working_hour_db[o]['OBJECT'] = 0
+                props.working_hour_db[o]['EDIT'] = 0
+
+        # 作業時間更新
+        active_obj = context.active_object
+        delta = self.__calc_delta(active_obj)
+        if active_obj.mode in ['OBJECT', 'EDIT']:
+            props.working_hour_db[active_obj.name][active_obj.mode] += delta
+
+
     def modal(self, context, event):
-        global working_hour
         props = context.scene.cwh_props
 
         # 3Dビューの画面を更新
@@ -89,14 +111,11 @@ class CalculateWorkingHours(bpy.types.Operator):
         if props.is_calc_mode is False:
             return {'FINISHED'}
 
-        update_db()
-
-        active_obj = context.active_object
-        delta = self.__calc_delta(active_obj)
-        if active_obj.mode in ['OBJECT', 'EDIT']:
-            working_hour[active_obj.name][active_obj.mode] += delta
+        # データベース更新
+        self.__update_db(context)
 
         return {'PASS_THROUGH'}
+
 
     def invoke(self, context, event):
         props = context.scene.cwh_props
@@ -123,8 +142,18 @@ class OBJECT_PT_CWH(bpy.types.Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
 
+
+    # 作業時間を表示用にフォーマット化
+    def __make_time_fmt(self, time):
+        msec = math.floor(time * 1000) % 1000
+        sec = math.floor(time) % 60
+        minute = math.floor(time / 60) % 60
+        hour = math.floor(time / (60 * 60))
+
+        return "%d:%02d:%02d.%d" % (hour, minute, sec, math.floor(msec / 100))
+
+
     def draw(self, context):
-        global working_hour
         sc = context.scene
         layout = self.layout
         props = sc.cwh_props
@@ -136,20 +165,28 @@ class OBJECT_PT_CWH(bpy.types.Panel):
 
         layout.separator()
 
+        # 作業時間の描画
         layout.prop(sc, "cwh_prop_object", text="オブジェクト")
         if sc.cwh_prop_object != "":
-            layout.label(text="オブジェクトモード：%.1f" % working_hour[sc.cwh_prop_object]['OBJECT'])
-            layout.label(text="エディットモード：%.1f" % working_hour[sc.cwh_prop_object]['EDIT'])
+            column = layout.column()
+            row = column.row()
+            row.label(text="オブジェクトモード")
+            row.label(text=self.__make_time_fmt(props.working_hour_db[sc.cwh_prop_object]['OBJECT']))
+            row = column.row()
+            row.label(text="エディットモード")
+            row.label(text=self.__make_time_fmt(props.working_hour_db[sc.cwh_prop_object]['EDIT']))
 
 
+# 作業時間を表示するオブジェクトを選択するための項目リストを作成
 def object_list_fn(scene, context):
-    global working_hour
+    props = context.scene.cwh_props
     items = [("", "", "")]
-    items.extend([(o, o, "") for o in working_hour.keys()])
+    items.extend([(o, o, "") for o in props.working_hour_db.keys()])
 
     return items
 
 
+# プロパティの作成
 def init_props():
     sc = bpy.types.Scene
     sc.cwh_prop_object = EnumProperty(
@@ -162,6 +199,7 @@ def init_props():
         type=CWH_Properties)
 
 
+# プロパティの削除
 def clear_props():
     sc = bpy.types.Scene
     del sc.cwh_prop_object
