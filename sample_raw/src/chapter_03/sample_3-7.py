7626fd26c96096e8d7b76eeed6b45310a48c45ea
Nutti <nutti.metro@gmail.com>
2016-12-17 23:37:25 +0900

    WIP: chapter 3 section 7

--- sample_raw/src/chapter_03/sample_3-7.py
+++ sample_raw/src/chapter_03/sample_3-7.py
@@ -21,15 +21,49 @@ bl_info = {
 translation_dict = {
     "en_US" :
         {
-            ("*", "Sample3-7: Delete Face By Right Click (Multilingual Support)") :
-                "Sample3-7: Delete Face By Right Click (Multilingual Support)",
-            ("*", "Sample3-7: Delete Face By Right Click (Multilingual Support)") :
-                "Sample3-7: Delete Face By Right Click (Multilingual Support)",
+            ("*", "Delete Face By Right Click") :
+                "Delete Face By Right Click",
+            ("*", "Sample3-7: Out of range") :
+                "Sample3-7: Out of range",
+            ("*", "Sample3-7: No face is selected") :
+                "Sample3-7: No face is selected",
+            ("*", "Sample3-7: Deleted Face") :
+                "Sample3-7: Deleted Face",
+            ("*", "Sample3-7: Start deleting faces") :
+                "Sample3-7: Start deleting faces",
+            ("*", "Sample3-7: %d face(s) are deleted") :
+                "Sample3-7: %d face(s) are deleted",
+            ("*", "Start") :
+                "Start",
+            ("*", "End") :
+                "End",
+            ("*", "Sample3-7: Enabled add-on 'Sample3-7'") :
+                "Sample3-7: Enabled add-on 'Sample3-7'",
+            ("*", "Sample3-7: Disabled add-on 'Sample3-7'") :
+                "Sample3-7: Disabled add-on 'Sample3-7'"
         },
     "ja_JP" :
         {
-            ("*", "Sample3-7: Delete Face By Right Click (Multilingual Support)") :
-                "サンプル3-7: マウスの右クリックで面を削除する（多言語対応版）"
+            ("*", "Delete Face By Right Click") :
+                "マウスの右クリックで面を削除",
+            ("*", "Sample3-7: Out of range") :
+                "サンプル3-7: 選択範囲外です。",
+            ("*", "Sample3-7: No face is selected") :
+                "サンプル3-7: 面以外を選択しました。",
+            ("*", "Sample3-7: Deleted Face") :
+                "サンプル3-7: 面を削除しました。",
+            ("*", "Sample3-7: Start deleting faces") :
+                "サンプル3-7: 削除処理を開始しました。",
+            ("*", "Sample3-7: %d face(s) are deleted") :
+                "サンプル3-7: %d個の面を削除しました。",
+            ("*", "Start") :
+                "開始",
+            ("*", "End") :
+                "終了",
+            ("*", "Sample3-7: Enabled add-on 'Sample3-7'") :
+                "サンプル3-7: アドオン「サンプル3-7」が有効化されました。",
+            ("*", "Sample3-7: Disabled add-on 'Sample3-7'") :
+                "サンプル3-7: アドオン「サンプル3-7」が無効化されました。"
         }
 }
 
@@ -56,8 +90,8 @@ class DFRC_Properties(bpy.types.PropertyGroup):
 # マウスの右クリックで面を削除
 class DeleteFaceByRClick(bpy.types.Operator):
     bl_idname = "mesh.delete_face_by_rclick"
-    bl_label = "マウスの右クリックで面を削除"
-    bl_description = "マウスの右クリックで面を削除します"
+    bl_label = bpy.app.translations.pgettext("Delete Face By Right Click")
+    bl_description = bpy.app.translations.pgettext("Delete Face By Right Click")
 
     def modal(self, context, event):
         props = context.scene.dfrc_props
@@ -68,7 +102,7 @@ class DeleteFaceByRClick(bpy.types.Operator):
 
         # 起動していない場合は終了
         if props.running is False:
-            return {'PASS_THROUGH'}
+            return {'FINISHED'}
 
         # クリック状態を更新
         if event.type == 'RIGHTMOUSE':
@@ -87,13 +121,13 @@ class DeleteFaceByRClick(bpy.types.Operator):
             loc = event.mouse_region_x, event.mouse_region_y
             ret = bpy.ops.view3d.select(location=loc)
             if ret == {'PASS_THROUGH'}:
-                print("サンプル3-1: 選択範囲外です。")
+                print(bpy.app.translations.pgettext("Sample3-7: Out of range"))
                 return {'PASS_THROUGH'}
             # 選択面を取得
             e = bm.select_history[-1]
             if not isinstance(e, bmesh.types.BMFace):
                 bm.select_history.remove(e)
-                print("サンプル3-1: 面以外を選択しました。")
+                print(bpy.app.translations.pgettext("Sample3-7: No face is selected"))
                 return {'PASS_THROUGH'}
             # 選択面を削除
             bm.select_history.remove(e)
@@ -104,7 +138,7 @@ class DeleteFaceByRClick(bpy.types.Operator):
             props.deleted_count = props.deleted_count + 1
             # マウスクリック中に連続して面が削除されることを防ぐ
             props.deleted = True
-            print("サンプル3-1: 面を削除しました。")
+            print(bpy.app.translations.pgettext("Sample3-7: Deleted Face"))
 
         # マウスがクリック状態から解除された時に、削除禁止状態を解除
         if props.right_mouse_down is False:
@@ -123,13 +157,13 @@ class DeleteFaceByRClick(bpy.types.Operator):
                 props.deleted_count = 0
                 # modal処理クラスを追加
                 context.window_manager.modal_handler_add(self)
-                print("サンプル3-1: 削除処理を開始しました。")
+                print(bpy.app.translations.pgettext("Sample3-7: Start deleting faces"))
                 return {'RUNNING_MODAL'}
             # 終了ボタンが押された時の処理
             else:
                 props.running = False
-                self.report({'INFO'}, "サンプル3-1: %d個の面を削除しました。" % (props.deleted_count))
-                print("サンプル3-1: %d個の面を削除しました。" % (props.deleted_count))
+                self.report({'INFO'}, bpy.app.translations.pgettext("Sample3-7: %d face(s) are deleted" % (props.deleted_count)))
+                print(bpy.app.translations.pgettext("Sample3-7: %d face(s) are deleted" % (props.deleted_count)))
                 return {'FINISHED'}
         else:
             return {'CANCELLED'}
@@ -137,9 +171,10 @@ class DeleteFaceByRClick(bpy.types.Operator):
 
 # UI
 class OBJECT_PT_DFRC(bpy.types.Panel):
-    bl_label = "マウスの右クリックで面を削除"
+    bl_label = bpy.app.translations.pgettext("Delete Face By Right Click")
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
+
 
     def draw(self, context):
         sc = context.scene
@@ -147,9 +182,9 @@ class OBJECT_PT_DFRC(bpy.types.Panel):
         props = context.scene.dfrc_props
         # 開始/停止ボタンを追加
         if props.running is False:
-            layout.operator(DeleteFaceByRClick.bl_idname, text="開始", icon="PLAY")
+            layout.operator(DeleteFaceByRClick.bl_idname, text=bpy.app.translations.pgettext("Start"), icon="PLAY")
         else:
-            layout.operator(DeleteFaceByRClick.bl_idname, text="終了", icon="PAUSE")
+            layout.operator(DeleteFaceByRClick.bl_idname, text=bpy.app.translations.pgettext("End"), icon="PAUSE")
 
 
 def register():
@@ -159,13 +194,17 @@ def register():
         name = "プロパティ",
         description = "本アドオンで利用するプロパティ一覧",
         type = DFRC_Properties)
-    print("サンプル3-1: アドオン「サンプル3-1」が有効化されました。")
+    # 辞書の登録
+    bpy.app.translations.register(__name__, translation_dict)
+    print(bpy.app.translations.pgettext("Sample3-7: Enabled add-on 'Sample3-7'"))
 
 
 def unregister():
+    # 辞書の削除
+    bpy.app.translations.unregister(__name__)
     del bpy.types.Scene.dfrc_props
     bpy.utils.unregister_module(__name__)
-    print("サンプル3-1: アドオン「サンプル3-1」が無効化されました。")
+    print(bpy.app.translations.pgettext("Sample3-7: Disabled add-on 'Sample3-7'"))
 
 
 if __name__ == "__main__":
