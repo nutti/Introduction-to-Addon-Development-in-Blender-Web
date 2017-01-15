4ff4b4b60434e3b1366eb446aa8f430f2e9a7e0a
Nutti <nutti.metro@gmail.com>
2017-01-15 20:50:21 +0900

    Update section 3-5: Simplify sample

--- sample/src/chapter_03/sample_3-5.py
+++ sample/src/chapter_03/sample_3-5.py
@@ -6,86 +6,61 @@ import math
 
 
 bl_info = {
-    "name": "サンプル3-5: オブジェクトモードとエディットモードでの作業時間を計測する（UI改善版）",
+    "name": "サンプル3-5: 3Dビューエリアにテキストを描画する",
     "author": "Nutti",
     "version": (2, 0),
     "blender": (2, 75, 0),
-    "location": "3Dビュー > プロパティパネル > 作業時間計測",
-    "description": "各オブジェクトについて、オブジェクトモードとエディットモードでの作業時間を計測するアドオン（UI改善版）",
+    "location": "3Dビュー > プロパティパネル > テキスト描画",
+    "description": "3Dビューエリアにテキストを描画するアドオン",
     "warning": "",
     "support": "TESTING",
     "wiki_url": "",
     "tracker_url": "",
-    "category": "System"
+    "category": "3D View"
 }
 
 
 # プロパティ
-class CWH_Properties(bpy.types.PropertyGroup):
-    is_calc_mode = BoolProperty(
-        name="作業時間計測中",
-        description="作業時間計測中か？",
+class RT_Properties(bpy.types.PropertyGroup):
+    running = BoolProperty(
+        name="テキスト描画中",
+        description="テキスト描画中か？",
         default=False)
-    working_hour_db = {}    # 作業時間を保存するためのデータベース
 
 
-# 作業時間計測時の処理
-class CalculateWorkingHours(bpy.types.Operator):
-    bl_idname = "ui.calculate_working_hours"
-    bl_label = "作業時間計測"
-    bl_description = "作業時間を計測します"
+# テキスト描画
+class RenderText(bpy.types.Operator):
+    bl_idname = "view_3d.render_text"
+    bl_label = "テキスト描画"
+    bl_description = "テキストを描画します"
 
     handle = None           # 描画関数ハンドラ
 
-    def __init__(self):
-        self.timer = None           # タイマハンドラ
-        self.prev_time = 0.0        # __calc_delta()メソッドを呼び出した時の時間
-        self.prev_obj = None        # __calc_delta()メソッドを呼び出した時に選択していたオブジェクト
-        self.prev_mode = None   # __calc_delta()メソッドを呼び出した時のモード
-
 
     def __handle_add(self, context):
-        if (self.timer is None) and (CalculateWorkingHours.handle is None):
-            # タイマを登録
-            self.timer = context.window_manager.event_timer_add(
-                0.10, context.window)
+        if RenderText.handle is None:
             # 描画関数の登録
-            CalculateWorkingHours.handler = bpy.types.SpaceView3D.draw_handler_add(
-                CalculateWorkingHours.render_working_hours, (self, context), 'WINDOW', 'POST_PIXEL')
+            RenderText.handle = bpy.types.SpaceView3D.draw_handler_add(
+                RenderText.render, (self, context), 'WINDOW', 'POST_PIXEL')
             # モーダルモードへの移行
             context.window_manager.modal_handler_add(self)
 
 
     def __handle_remove(self, context):
-        if CalculateWorkingHours.handler is not None:
+        if RenderText.handle is not None:
             # 描画関数の登録を解除
-            bpy.types.SpaceView3D.draw_handler_remove(CalculateWorkingHours.handler, 'WINDOW')
-            CalculateWorkingHours.handler = None
-        if self.timer is not None:
-            # タイマの登録を解除
-            context.window_manager.event_timer_remove(self.timer)
-            self.timer = None
-
-
-    # 作業時間を表示用にフォーマット化
-    @staticmethod
-    def make_time_fmt(time):
-        msec = math.floor(time * 1000) % 1000   # ミリ秒
-        sec = math.floor(time) % 60                     # 秒
-        minute = math.floor(time / 60) % 60         # 分
-        hour = math.floor(time / (60 * 60))           # 時
-
-        return "%d:%02d:%02d.%d" % (hour, minute, sec, math.floor(msec / 100))
+            bpy.types.SpaceView3D.draw_handler_remove(RenderText.handle, 'WINDOW')
+            RenderText.handle = None
 
 
     @staticmethod
-    def render_message(size, x, y, msg):
+    def render_text(size, x, y, s):
         # フォントサイズを指定
         blf.size(0, size, 72)
         # 描画位置を指定
         blf.position(0, x, y, 0)
-        # 文字列を描画
-        blf.draw(0, msg)
+        # テキストを描画
+        blf.draw(0, s)
 
 
     @staticmethod
@@ -105,18 +80,11 @@ class CalculateWorkingHours(bpy.types.Operator):
 
 
     @staticmethod
-    def render_working_hours(self, context):
-        sc = context.scene
-        props = sc.cwh_props
-
-        # 表示するオブジェクトが選択されていない場合は、描画しない
-        if sc.cwh_prop_object == '':
-            return
-
+    def render(self, context):
         # リージョン幅を取得するため、描画先のリージョンを得る
-        region = CalculateWorkingHours.get_region(context, 'VIEW_3D', 'WINDOW')
+        region = RenderText.get_region(context, 'VIEW_3D', 'WINDOW')
 
-        # 描画先のリージョンへ文字列を描画
+        # 描画先のリージョンへテキストを描画
         if region is not None:
             # 影の効果を設定
             blf.shadow(0, 3, 0.0, 1.0, 0.0, 0.5)
@@ -124,97 +92,47 @@ class CalculateWorkingHours(bpy.types.Operator):
             blf.shadow_offset(0, 2, -2)
             # 影の効果を有効化
             blf.enable(0, blf.SHADOW)
-            CalculateWorkingHours.render_message(20, 20, region.height - 60, "Working Hour")
+            RenderText.render_text(40, 40, region.height - 120, "Hello Blender world!!")
             # 影の効果を無効化
             blf.disable(0, blf.SHADOW)
-            CalculateWorkingHours.render_message(15, 20, region.height - 90, "Object: " + sc.cwh_prop_object)
-            CalculateWorkingHours.render_message(15, 20, region.height - 115,
-                "Object Mode: " + CalculateWorkingHours.make_time_fmt(props.working_hour_db[sc.cwh_prop_object]['OBJECT']))
-            CalculateWorkingHours.render_message(15, 20, region.height - 135, "Edit Mode: " + CalculateWorkingHours.make_time_fmt(props.working_hour_db[sc.cwh_prop_object]['EDIT']))
-
-
-    # 前回の呼び出しからの時間差分を計算
-    def __calc_delta(self, obj):
-        # 現在時刻を取得
-        cur_time = datetime.datetime.now()
-
-        # オブジェクトやモードが異なっていた場合は無効とし、時間差分を0とする
-        if (self.prev_obj != obj) or (self.prev_mode != obj.mode):
-            delta = 0.0
-        else:
-            delta = (cur_time - self.prev_time).total_seconds()
-
-        # 情報をアップデート
-        self.prev_time = cur_time
-        self.prev_obj = obj
-        self.prev_mode = obj.mode
-
-        return delta
-
-
-    # データベースを更新
-    def __update_db(self, context):
-        props = context.scene.cwh_props
-
-        # 全てのメッシュ型オブジェクトの取得
-        obj_list = [obj.name for obj in bpy.data.objects if obj.type == 'MESH']
-        # データベースに存在しないオブジェクトをデータベースに追加
-        for o in obj_list:
-            if not o in props.working_hour_db.keys():
-                props.working_hour_db[o] = {}
-                props.working_hour_db[o]['OBJECT'] = 0
-                props.working_hour_db[o]['EDIT'] = 0
-
-        # 作業時間更新
-        active_obj = context.active_object
-        delta = self.__calc_delta(active_obj)
-        if active_obj.mode in ['OBJECT', 'EDIT']:
-            props.working_hour_db[active_obj.name][active_obj.mode] += delta
+            RenderText.render_text(30, 40, region.height - 180, "Suzanne on your lap")
 
 
     def modal(self, context, event):
-        props = context.scene.cwh_props
-
-        # タイマイベント以外の場合は無視
-        if event.type != 'TIMER':
-            return {'PASS_THROUGH'}
-
+        props = context.scene.rt_props
         # 3Dビューの画面を更新
         if context.area:
             context.area.tag_redraw()
 
         # 作業時間計測を停止
-        if props.is_calc_mode is False:
+        if props.running is False:
             self.__handle_remove(context)
             return {'FINISHED'}
-
-        # データベース更新
-        self.__update_db(context)
 
         return {'PASS_THROUGH'}
 
 
     def invoke(self, context, event):
-        props = context.scene.cwh_props
+        props = context.scene.rt_props
         if context.area.type == 'VIEW_3D':
             # 開始ボタンが押された時の処理
-            if props.is_calc_mode is False:
-                props.is_calc_mode = True
+            if props.running is False:
+                props.running = True
                 self.__handle_add(context)
-                print("サンプル3-5: 作業時間の計測を開始しました。")
+                print("サンプル3-5: テキストの描画を開始しました。")
                 return {'RUNNING_MODAL'}
             # 終了ボタンが押された時の処理
             else:
-                props.is_calc_mode = False
-                print("サンプル3-5: 作業時間の計測を終了しました。")
+                props.running = False
+                print("サンプル3-5: テキストの描画を終了しました。")
                 return {'FINISHED'}
         else:
             return {'CANCELLED'}
 
 
 # UI
-class OBJECT_PT_CWH(bpy.types.Panel):
-    bl_label = "作業時間計測"
+class OBJECT_PT_RT(bpy.types.Panel):
+    bl_label = "テキスト描画"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
 
@@ -222,46 +140,27 @@ class OBJECT_PT_CWH(bpy.types.Panel):
     def draw(self, context):
         sc = context.scene
         layout = self.layout
-        props = sc.cwh_props
+        props = sc.rt_props
         # 開始/停止ボタンを追加
-        if props.is_calc_mode is False:
-            layout.operator(CalculateWorkingHours.bl_idname, text="開始", icon="PLAY")
+        if props.running is False:
+            layout.operator(RenderText.bl_idname, text="開始", icon="PLAY")
         else:
-            layout.operator(CalculateWorkingHours.bl_idname, text="終了", icon="PAUSE")
-
-        layout.separator()
-
-        # 作業時間の描画
-        layout.prop(sc, "cwh_prop_object", text="オブジェクト")
-
-
-# 作業時間を表示するオブジェクトを選択するための項目リストを作成
-def object_list_fn(scene, context):
-    props = context.scene.cwh_props
-    items = [("", "", "")]
-    items.extend([(o, o, "") for o in props.working_hour_db.keys()])
-
-    return items
+            layout.operator(RenderText.bl_idname, text="終了", icon="PAUSE")
 
 
 # プロパティの作成
 def init_props():
     sc = bpy.types.Scene
-    sc.cwh_prop_object = EnumProperty(
-        name="オブジェクト",
-        description="作業時間を表示する対象のオブジェクト",
-        items=object_list_fn)
-    sc.cwh_props = PointerProperty(
+    sc.rt_props = PointerProperty(
         name="プロパティ",
         description="本アドオンで利用するプロパティ一覧",
-        type=CWH_Properties)
+        type=RT_Properties)
 
 
 # プロパティの削除
 def clear_props():
     sc = bpy.types.Scene
-    del sc.cwh_prop_object
-    del sc.cwh_props
+    del sc.rt_props
 
 
 def register():
