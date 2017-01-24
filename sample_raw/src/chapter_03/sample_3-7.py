b01fb153dad8dd95f8db12474a97bfacc803f760
Nutti <nutti.metro@gmail.com>
2017-01-24 23:14:43 +0900

    Update section 3-7: Add description about pgettext_iface

--- sample_raw/src/chapter_03/sample_3-7.py
+++ sample_raw/src/chapter_03/sample_3-7.py
@@ -160,8 +160,10 @@ class DeleteFaceByRClick(bpy.types.Operator):
             # 終了ボタンが押された時の処理
             else:
                 props.running = False
-                self.report({'INFO'}, bpy.app.translations.pgettext("Sample3-7: %d face(s) are deleted" % (props.deleted_count)))
-                print(bpy.app.translations.pgettext("Sample3-7: %d face(s) are deleted" % (props.deleted_count)))
+                self.report({'INFO'}, bpy.app.translations.pgettext_iface("Sample3-7: %d face(s) are deleted") % (props.deleted_count))
+//! [translation_func_with_format]
+                print(bpy.app.translations.pgettext_iface("Sample3-7: %d face(s) are deleted") % (props.deleted_count))
+//! [translation_func_with_format]
                 return {'FINISHED'}
         else:
             return {'CANCELLED'}
