f1676d1ad2844eb1807971cba7aad63d2123e32a
Nutti <nutti.metro@gmail.com>
2017-03-11 21:00:36 +0900

    testing

--- tests/install_sample.sh
+++ tests/install_sample.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: install_sample.sh <target>"
+if [ $# -ne 2 ]; then
+    echo "Usage: install_sample.sh <source> <target>"
 fi
 
+src=${1}
+tgt=${2}
 
 # search package
-package=()
+packages=()
 files=()
-for file in `\find ./sample -type f -name '*.py'`; do
+for file in `\find ${src} -type f -name '*.py'`; do
     if [ `echo "${file}" | grep '__init__.py'` ]; then
         path=${file%/*}
-        package+=("${path}")
-        
+        packages+=("${path}")
+
     fi
     files+=("${file}")
 done
 
 # remove package
-module=()
+modules=()
 for file in ${files[@]}; do
     found=0
-    for pkg in ${package[@]}; do
+    for pkg in ${packages[@]}; do
         if [ `echo "${file}" | grep "${pkg}"` ]; then
             found=1
         fi
     done
     if [ ${found} -ne 1 ]; then
-        module+=("${file}")
+        modules+=("${file}")
     fi
 done
 
-echo "pa"
-echo ${package[@]}
-echo "mo"
-echo ${module[@]}
+# copy packages to target
+for pkg in ${packages[@]}; do
+    cp -r ${pkg} ${tgt}
+    echo "Copied "${pkg}" to "${tgt}
+done
 
+# copy modules to target
+for mod in ${modules[@]}; do
+    cp ${mod} ${tgt}
+    echo "Copied "${mod}" to "${tgt}
+done
